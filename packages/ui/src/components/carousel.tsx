904516571860ba8a449558085a0dd1f2635c2afc
t-hash06 <tomasfelipe.p@gmail.com>
2026-03-07 15:45:56 -0500

    chore: bump packages/ui deps

--- packages/ui/src/components/carousel.tsx
+++ packages/ui/src/components/carousel.tsx
@@ -116,14 +116,16 @@ function Carousel({
 				canScrollNext,
 			}}
 		>
-			<section
+			<div
 				onKeyDownCapture={handleKeyDown}
 				className={cn('relative', className)}
+				role="region"
+				aria-roledescription="carousel"
 				data-slot="carousel"
 				{...props}
 			>
 				{children}
-			</section>
+			</div>
 		</CarouselContext.Provider>
 	);
 }
@@ -149,14 +151,12 @@ function CarouselContent({ className, ...props }: React.ComponentProps<'div'>) {
 	);
 }
 
-function CarouselItem({
-	className,
-	...props
-}: React.ComponentProps<'fieldset'>) {
+function CarouselItem({ className, ...props }: React.ComponentProps<'div'>) {
 	const { orientation } = useCarousel();
 
 	return (
-		<fieldset
+		<div
+			role="group"
 			aria-roledescription="slide"
 			data-slot="carousel-item"
 			className={cn(
@@ -183,7 +183,7 @@ function CarouselPrevious({
 			variant={variant}
 			size={size}
 			className={cn(
-				'rounded-full absolute touch-manipulation',
+				'absolute touch-manipulation rounded-full',
 				orientation === 'horizontal'
 					? 'top-1/2 -left-12 -translate-y-1/2'
 					: '-top-12 left-1/2 -translate-x-1/2 rotate-90',
@@ -213,7 +213,7 @@ function CarouselNext({
 			variant={variant}
 			size={size}
 			className={cn(
-				'rounded-full absolute touch-manipulation',
+				'absolute touch-manipulation rounded-full',
 				orientation === 'horizontal'
 					? 'top-1/2 -right-12 -translate-y-1/2'
 					: '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
--- packages/ui/src/components/slider.tsx
+++ packages/ui/src/components/slider.tsx
@@ -31,10 +31,10 @@ function Slider({
 			thumbAlignment="edge"
 			{...props}
 		>
-			<SliderPrimitive.Control className="data-vertical:min-h-40 relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:w-auto data-vertical:flex-col">
+			<SliderPrimitive.Control className="relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col">
 				<SliderPrimitive.Track
 					data-slot="slider-track"
-					className="bg-muted rounded-full data-horizontal:h-1 data-horizontal:w-full data-vertical:h-full data-vertical:w-1 relative grow overflow-hidden select-none"
+					className="relative grow overflow-hidden rounded-full bg-muted select-none data-horizontal:h-1 data-horizontal:w-full data-vertical:h-full data-vertical:w-1"
 				>
 					<SliderPrimitive.Indicator
 						data-slot="slider-range"
@@ -46,7 +46,7 @@ function Slider({
 						data-slot="slider-thumb"
 						// biome-ignore lint/suspicious/noArrayIndexKey: Because the thumbs are static and won't be added/removed dynamically, using the index as key is acceptable here.
 						key={index}
-						className="border-ring ring-ring/50 relative size-3 rounded-full border bg-white transition-[color,box-shadow] after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 block shrink-0 select-none disabled:pointer-events-none disabled:opacity-50"
+						className="relative block size-3 shrink-0 rounded-full border border-ring bg-white ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 disabled:pointer-events-none disabled:opacity-50"
 					/>
 				))}
 			</SliderPrimitive.Control>
